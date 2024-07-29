974486e3588ade83034f590a386f08c6bd224512
Tom <JosephDotson@belkonar.com>
2024-07-28 21:29:38 -0400

    main setup

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -29,39 +29,6 @@ return {
 		end,
 	},
 	{
-		"williamboman/mason.nvim",
-		opts = { ensure_installed = { "csharpier", "netcoredbg" } },
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				omnisharp = {
-					handlers = {
-						["textDocument/definition"] = function(...)
-							return require("omnisharp_extended").handler(...)
-						end,
-					},
-					keys = {
-						{
-							"gd",
-							function()
-								require("omnisharp_extended").telescope_lsp_definitions()
-							end,
-							desc = "Goto Definition",
-						},
-					},
-					enable_roslyn_analyzers = true,
-					organize_imports_on_format = true,
-					enable_import_completion = true,
-				},
-			},
-		},
-	},
-	{
-		"Issafalcon/neotest-dotnet",
-	},
-	{
 		"nvimtools/none-ls.nvim",
 		optional = true,
 		opts = function(_, opts)
